adb75ffc1522c11ad24f96af11a3139ec30db2dc
ethanostic <ethanclausing@gmail.com>
2024-11-24 08:47:21 +0300

    Create maze_generator_terminal-based.py
    Created the maze_generator file. Started on making the maze size and the set walls. Did not make the random maze generator. Needs a GUI, maybe Tkinter. Only works for terminal right now.

---
+++ maze_generator_terminal-based.py
@@ -0,0 +1,54 @@
+#region Generate maze size
+
+# Imports and pre-defined variables
+import json
+y_dict = {}
+maze_dict = {}
+
+# Odd number input greater than or equal to 5 for x
+while True:
+    x_length = int(input("X: "))
+    if x_length >= 5:
+        if x_length % 2 != 0:
+            break
+x_list = list(range(1, x_length + 1))
+
+# Odd number input greater than or equal to 5 for y
+while True:
+    y_length = int(input("Y: "))
+    if y_length >= 5:
+        if y_length % 2 != 0:
+            break
+y_list = list(range(1, y_length + 1))
+for number in y_list:
+    y_dict[number] = "path"
+    
+
+# generate maze
+for number in x_list:
+    maze_dict[number] = y_dict.copy()
+
+#endregion
+
+#region Set standard maze walls
+
+# Sets borders
+for x_key, y_dict in maze_dict.items():
+    for y_key in y_dict:
+        if x_key == 1 or x_key == x_length:
+            maze_dict[x_key][y_key] = "wall"
+        elif y_key == 1 or y_key == y_length:
+            maze_dict[x_key][y_key] = "wall"
+
+# Sets pillars
+for x_key, y_dict in maze_dict.items():
+    for y_key in y_dict:
+        if x_key % 2 != 0:
+            if y_key % 2 != 0:
+                maze_dict[x_key][y_key] = "wall"
+
+#endregion
+
+# fill in maze
+
+print(json.dumps(maze_dict, indent=4))
