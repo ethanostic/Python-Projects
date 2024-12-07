b76000ac2c45a292545a64bfa88b8823324d44c0
ethanostic <ethanclausing@gmail.com>
2024-12-07 11:12:21 +0300

    Delete snake_movement.py
    Deleted snake_movement as there is a better one one the snake folder

--- snake_movement.py
+++
@@ -1,8 +0,0 @@
-#def snake_movement()
-    #'''Changes the snakes position by adding one cell to the head and 
-    # remove one cell from the tail'''
-
-    # if a cell is not facing a snake cell. then fill the
-    # appropriate cell with a snake cell
-
-    # if a cell is not being faced, then remove that snake cell
